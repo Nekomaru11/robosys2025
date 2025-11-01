2e56cb63d4d15a6fbac408e6fb4468f86ce1ec8f
Endo Shuma <s24c1023rt@s.chibakoudai.jp>
2025-11-02 04:14:01 +0900

    Add regression test script test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
-　
+## SPDX-FileCopyrightText: 2025 Shuma Endo <s24c1023rt@s.chibakoudai.jp>
+## SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
-        echo ${1}行目が違うよ
-        res=1                   #追加
+      echo "${1}行目が違うよ"
+       res=1
 }
-　
+
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数         
-[ "$a" = 山田 ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
-　
-exit $res     # このシェルスクリプトの終了ステータスを返して終了
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo "OK" # 通ったのが分かるように表示
+exit $res
